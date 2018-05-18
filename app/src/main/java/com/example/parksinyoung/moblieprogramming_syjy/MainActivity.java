7c9a8ed10bde70c29de2efb03680ce37e4922d39
Park Sin Young <qkrtlsdud428@gmail.com>
2018-05-18 22:16:55 +0900

    bottom navigation(selector)-ui

--- app/src/main/java/com/example/parksinyoung/moblieprogramming_syjy/MainActivity.java
+++ app/src/main/java/com/example/parksinyoung/moblieprogramming_syjy/MainActivity.java
@@ -15,6 +15,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mBottomNavigationView = findViewById(R.id.main_bnv);
+
+        BottomNavigationViewHelper.removeShiftMode(mBottomNavigationView);
         mBottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
