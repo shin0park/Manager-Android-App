50c85027b8b95bc970ce593e6475e3beb515e288
Park Sin Young <qkrtlsdud428@gmail.com>
2018-05-18 20:09:21 +0900

    사랑해2

--- app/src/main/java/com/example/parksinyoung/moblieprogramming_syjy/MainActivity.java
+++ app/src/main/java/com/example/parksinyoung/moblieprogramming_syjy/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("신주연 바보");
+        System.out.println("신주연 바보!");
     }
 }
