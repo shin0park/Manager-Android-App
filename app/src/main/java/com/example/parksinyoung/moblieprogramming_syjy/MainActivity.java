5f6251290586cb97f9d919fc3d051e3b93f0e1ff
Park Sin Young <qkrtlsdud428@gmail.com>
2018-05-18 21:16:22 +0900

    bottom navigation-ui

--- app/src/main/java/com/example/parksinyoung/moblieprogramming_syjy/MainActivity.java
+++ app/src/main/java/com/example/parksinyoung/moblieprogramming_syjy/MainActivity.java
@@ -1,14 +1,38 @@
 package com.example.parksinyoung.moblieprogramming_syjy;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+    private BottomNavigationView mBottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("신주연 바보!!");
+
+        mBottomNavigationView = findViewById(R.id.main_bnv);
+        mBottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.action_one:
+                        return true;
+                    case R.id.action_two:
+                        return true;
+                    case R.id.action_three:
+                        return true;
+                    case R.id.action_four:
+                        return true;
+                    case R.id.action_five:
+                        return true;
+                }
+                return false;
+            }
+        });
+
     }
 }
