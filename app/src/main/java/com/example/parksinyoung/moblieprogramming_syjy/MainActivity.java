0dde5b7ef0d3776dca2976714d9e269fa8a9824e
Park Sin Young <qkrtlsdud428@gmail.com>
2018-05-20 23:47:16 +0900

    화면 넘김-로그인 성공시 MainActivity 화면으로 이동 구현 완료 actionbar에 버튼 추가-설정 버튼(로그아웃 추가 예정)

--- app/src/main/java/com/example/parksinyoung/moblieprogramming_syjy/MainActivity.java
+++ app/src/main/java/com/example/parksinyoung/moblieprogramming_syjy/MainActivity.java
@@ -4,7 +4,10 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -34,6 +37,27 @@ public class MainActivity extends AppCompatActivity {
                 return false;
             }
         });
-
     }
+//action bar에 있는 설정 메뉴
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // inflater함수를 이용해서 menu 리소스를 menu로 변환.
+        getMenuInflater().inflate(R.menu.menu_action_bar, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+    //action bar에 있는 설정메뉴 클릭시
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // TODO Auto-generated method stub
+        switch (item.getItemId()) {
+            case R.id.setting_btn:
+                // setting_btn 이 눌렸을 경우 이벤트 발생
+                Toast.makeText(getApplicationContext(),"setting 버튼 클릭.",Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+
 }
