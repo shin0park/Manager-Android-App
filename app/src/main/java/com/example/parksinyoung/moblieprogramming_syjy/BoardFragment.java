85e885d8f1dfa5ea9c781b909908054351b71bdc
user <wndus6165@gmail.com>
2018-05-22 20:16:58 +0900

    pull 오류

--- app/src/main/java/com/example/parksinyoung/moblieprogramming_syjy/BoardFragment.java
+++ app/src/main/java/com/example/parksinyoung/moblieprogramming_syjy/BoardFragment.java
@@ -2,15 +2,17 @@ package com.example.parksinyoung.moblieprogramming_syjy;
 
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class BoardFragment extends Fragment {
+public class BoardFragment extends Fragment implements NavigationView.OnNavigationItemSelectedListener{
 
     @NonNull
     public static BoardFragment newInstance() {
@@ -26,5 +28,10 @@ public class BoardFragment extends Fragment {
         actionBar.setDisplayHomeAsUpEnabled(true); // 뒤로가기 버튼, 디폴트로 true만 해도 백버튼이 생김
        // actionBar.setHomeAsUpIndicator(R.drawable.arrow); //뒤로가기 버튼을 본인이 만든 아이콘으로 하기 위해 필요
         return view;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected( @NonNull MenuItem item ) {
+        return false;
     }
 }
