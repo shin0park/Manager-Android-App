d2387f5ae55d4c2bf40452e88278cdbfda217d4e
user <wndus6165@gmail.com>
2018-06-06 18:07:04 +0900

    캘린더수정

--- app/src/main/java/com/example/parksinyoung/moblieprogramming_syjy/BoardFragment.java
+++ app/src/main/java/com/example/parksinyoung/moblieprogramming_syjy/BoardFragment.java
@@ -3,11 +3,18 @@ package com.example.parksinyoung.moblieprogramming_syjy;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.ActionBar;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class BoardFragment extends ToolBarFragment {
+    private ActionBar actionBar;
+    private TextView toolbarText;
 
     @NonNull
     public static BoardFragment newInstance() {
