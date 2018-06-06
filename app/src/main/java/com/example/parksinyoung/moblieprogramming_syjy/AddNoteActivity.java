9be64f41635f3da99a58085a8a8305bb77677776
user <wndus6165@gmail.com>
2018-06-06 14:26:13 +0900

    캘린더 구현

---
+++ app/src/main/java/com/example/parksinyoung/moblieprogramming_syjy/AddNoteActivity.java
@@ -0,0 +1,32 @@
+package com.example.parksinyoung.moblieprogramming_syjy;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import com.applandeo.materialcalendarview.CalendarView;
+
+public class AddNoteActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note_write);
+        final CalendarView datePicker = (CalendarView) findViewById(R.id.datePicker);
+        Button button = (Button) findViewById(R.id.addNoteButton);
+        final EditText noteEditText = (EditText) findViewById(R.id.noteEditText);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent returnIntent = new Intent();
+                MyEventDay myEventDay = new MyEventDay(datePicker.getSelectedDate(),
+                        R.drawable.ic_message_black_48dp, noteEditText.getText().toString());
+                returnIntent.putExtra(CalendarActivity.RESULT, myEventDay);
+                setResult(Activity.RESULT_OK, returnIntent);
+                finish();
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/parksinyoung/moblieprogramming_syjy/CalendarActivity.java
@@ -0,0 +1,60 @@
+package com.example.parksinyoung.moblieprogramming_syjy;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import com.applandeo.materialcalendarview.CalendarView;
+import com.applandeo.materialcalendarview.EventDay;
+import com.applandeo.materialcalendarview.listeners.OnDayClickListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CalendarActivity extends AppCompatActivity {
+    public static final String RESULT = "result";
+    public static final String EVENT = "event";
+    private static final int ADD_NOTE = 44;
+    private CalendarView mCalendarView;
+    private List<EventDay> mEventDays = new ArrayList<>();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calender);
+        mCalendarView = (CalendarView) findViewById(R.id.calendarView);
+        FloatingActionButton floatingActionButton = (FloatingActionButton) findViewById(R.id.floatingActionButton);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addNote();
+            }
+        });
+        mCalendarView.setOnDayClickListener(new OnDayClickListener() {
+            @Override
+            public void onDayClick(EventDay eventDay) {
+                previewNote(eventDay);
+            }
+        });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == ADD_NOTE && resultCode == RESULT_OK) {
+            MyEventDay myEventDay = data.getParcelableExtra(RESULT);
+            mCalendarView.setDate(myEventDay.getCalendar());
+            mEventDays.add(myEventDay);
+            mCalendarView.setEvents(mEventDays);
+        }
+    }
+    private void addNote() {
+        Intent intent = new Intent(this, AddNoteActivity.class);
+        startActivityForResult(intent, ADD_NOTE);
+    }
+    private void previewNote(EventDay eventDay) {
+        Intent intent = new Intent(this, NotePreviewActivity.class);
+        if(eventDay instanceof MyEventDay){
+            intent.putExtra(EVENT, (MyEventDay) eventDay);
+        }
+        startActivity(intent);
+    }
+}
