f43d6fb3168ef3c9959c3e36c839c7718646c31a
user <wndus6165@gmail.com>
2018-06-12 06:21:45 +0900

    홈구현

---
+++ app/src/main/java/com/example/parksinyoung/moblieprogramming_syjy/Dday.java
@@ -0,0 +1,23 @@
+package com.example.parksinyoung.moblieprogramming_syjy;
+
+import java.util.Calendar;
+
+public class Dday {
+    public int caldate(int myear, int mmonth, int mday) {
+        try {
+            Calendar today = Calendar.getInstance();//현재 오늘 날짜
+            Calendar dday = Calendar.getInstance(); dday.set(myear,mmonth,mday);
+
+            // D-day의 날짜를 입력합니다.
+            long day = dday.getTimeInMillis()/86400000;
+            // 각각 날의 시간 값을 얻어온 다음
+            // ( 1일의 값(86400000 = 24시간 * 60분 * 60초 * 1000(1초값) ) )
+            long tday = today.getTimeInMillis()/86400000; long count = tday - day;
+            // 오늘 날짜에서 dday 날짜를 빼주게 됩니다.
+            return (int) count+1;
+            // 날짜는 하루 + 시켜줘야합니다.
+            } catch (Exception e)
+        { e.printStackTrace(); return -1;
+            }
+    }
+}
--- app/src/main/java/com/example/parksinyoung/moblieprogramming_syjy/HomeFragment.java
+++ app/src/main/java/com/example/parksinyoung/moblieprogramming_syjy/HomeFragment.java
@@ -1,5 +1,7 @@
 package com.example.parksinyoung.moblieprogramming_syjy;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -11,7 +13,11 @@ import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class HomeFragment extends ToolBarFragment {
 
@@ -20,11 +26,62 @@ public class HomeFragment extends ToolBarFragment {
 //        return new HomeFragment();
 //    }
 
+    private TextView ddayText;
+    private TextView todayText;
+    private TextView resultText;
+    private Button dateButton;
+
+    private int tYear;           //오늘 연월일 변수
+    private int tMonth;
+    private int tDay;
+
+    private int dYear=1;        //디데이 연월일 변수
+    private int dMonth=1;
+    private int dDay=1;
+
+
+    private long d;
+    private long t;
+    private long r;
+
+    private int resultNumber=0;
+
+    static final int DATE_DIALOG_ID=0;
+
+
     @Nullable
     @Override
     public View onCreateView( LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState ) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
         setToolbar();
+
+        ddayText=view.findViewById(R.id.dday);
+        todayText=(TextView)view.findViewById(R.id.today);
+        resultText=(TextView)view.findViewById(R.id.result);
+        dateButton=(Button)view.findViewById(R.id.dateButton);
+        dateButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                getActivity().showDialog(0);
+            }
+        });
+        Calendar calendar =Calendar.getInstance();              //현재 날짜 불러옴
+        tYear = calendar.get(Calendar.YEAR);
+        tMonth = calendar.get(Calendar.MONTH);
+        tDay = calendar.get(Calendar.DAY_OF_MONTH);
+
+        Calendar dCalendar =Calendar.getInstance();
+        dCalendar.set(dYear,dMonth, dDay);
+
+        t=calendar.getTimeInMillis();                 //오늘 날짜를 밀리타임으로 바꿈
+        d=dCalendar.getTimeInMillis();              //디데이날짜를 밀리타임으로 바꿈
+        r=(d-t)/(24*60*60*1000);                 //디데이 날짜에서 오늘 날짜를 뺀 값을 '일'단위로 바꿈
+
+        resultNumber=(int)r+1;
+        updateDisplay();
+
         return view;
     }
 
@@ -38,5 +95,53 @@ public class HomeFragment extends ToolBarFragment {
         }
         return super.onOptionsItemSelected(item);
     }
+    private void updateDisplay(){
+
+        todayText.setText(String.format("%d년 %d월 %d일",tYear, tMonth+1,tDay));
+        ddayText.setText(String.format("%d년 %d월 %d일",dYear, dMonth+1,dDay));
+
+        if(resultNumber>=0){
+            resultText.setText(String.format("D-%d", resultNumber));
+        }
+        else{
+            int absR=Math.abs(resultNumber);
+            resultText.setText(String.format("D+%d", absR));
+        }
+    }//디데이 날짜가 오늘날짜보다 뒤에오면 '-', 앞에오면 '+'를 붙인다
+
+    private DatePickerDialog.OnDateSetListener dDateSetListener=new DatePickerDialog.OnDateSetListener() {
+
+        @Override
+        public void onDateSet( DatePicker view, int year, int monthOfYear,
+                               int dayOfMonth) {
+            // TODO Auto-generated method stub
+            dYear=year;
+            dMonth=monthOfYear;
+            dDay=dayOfMonth;
+            final Calendar dCalendar =Calendar.getInstance();
+            dCalendar.set(dYear,dMonth, dDay);
+
+            d=dCalendar.getTimeInMillis();
+            r=(d-t)/(24*60*60*1000);
+
+            resultNumber=(int)r;
+            updateDisplay();
+        }
+    };
+
+
+    @Override
+    protected Dialog onCreateDialog( int id){
+        if(id==DATE_DIALOG_ID){
+            return new DatePickerDialog(this,dDateSetListener,tYear,tMonth,tDay);
+
+        }
+        return null;
+    }
+
+
+
 }
+
+
 
