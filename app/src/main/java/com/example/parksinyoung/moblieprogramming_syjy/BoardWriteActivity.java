ed44f88b917d4809eaa4968a750236651c9fb525
user <wndus6165@gmail.com>
2018-06-12 13:56:37 +0900

    전체수정

--- app/src/main/java/com/example/parksinyoung/moblieprogramming_syjy/BoardWriteActivity.java
+++ app/src/main/java/com/example/parksinyoung/moblieprogramming_syjy/BoardWriteActivity.java
@@ -76,6 +76,7 @@ public class BoardWriteActivity extends AppCompatActivity {
         }
 
 
+
         writeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
